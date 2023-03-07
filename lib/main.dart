1da53810d61603f240499b4f4bf0671d6a0bbf54
Artem <artem.parshin.94@gmail.com>
2023-03-07 17:21:01 +0200

    project structure fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'calendar_tab.dart';
+import 'views/calendar_tab.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
